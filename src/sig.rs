f3d3271b5e14852e8531751b097813cb65a67092
Leon Tan <leon.arian.tan@gmail.com>
2024-08-11 15:36:31 +0200

    WIP

--- src/sig.rs
+++ src/sig.rs
@@ -1,10 +1,14 @@
 //! Implementation of the basic signature scheme used in the protocol
 
-use ark_bls12_381::{Fr, G1Affine};
-use ark_ec::AffineRepr;
-use ark_ff::UniformRand;
+use ark_bls12_381::{Fr, G1Affine, G1Projective, G2Affine, G2Projective};
+use ark_ec::{
+    hashing::{curve_maps::wb::WBMap, map_to_curve_hasher::MapToCurveBasedHasher, HashToCurve},
+    AffineRepr,
+};
+use ark_ff::{field_hashers::DefaultFieldHasher, UniformRand};
 use ark_serialize::{CanonicalDeserialize, CanonicalSerialize};
 use rand::{CryptoRng, RngCore};
+use sha2::Sha256;
 use zeroize::Zeroize;
 
 #[derive(Zeroize, Clone, CanonicalSerialize, CanonicalDeserialize)]
@@ -13,8 +17,11 @@ pub struct SecretKey(Fr);
 #[derive(Zeroize, Clone, CanonicalSerialize, CanonicalDeserialize)]
 pub struct PublicKey(G1Affine);
 
+#[derive(Zeroize, Clone, CanonicalSerialize, CanonicalDeserialize)]
+pub struct Signature(G2Affine);
+
 // TODO: Proof of possession
-// TODO: Sign
+// TODO: Verify proof of possesion
 // TODO: Verify
 
 impl SecretKey {
@@ -24,5 +31,22 @@ impl SecretKey {
 
     pub fn pub_key(&self) -> PublicKey {
         PublicKey((G1Affine::generator() * self.0).into())
+    }
+
+    pub fn sign<M: AsRef<[u8]>>(&self, message: M) -> Signature {
+        let hasher: MapToCurveBasedHasher<G2Projective, DefaultFieldHasher<Sha256>, WBMap<_>> =
+            MapToCurveBasedHasher::new(b"sign").unwrap();
+        let fm: G2Affine = hasher.hash(message.as_ref()).unwrap();
+        Signature((fm * self.0).into())
+    }
+}
+
+impl PublicKey {
+    pub fn verify<M: AsRef<[u8]>>(&self, message: M, signature: &Signature) -> bool {
+        let hasher: MapToCurveBasedHasher<G2Projective, DefaultFieldHasher<Sha256>, WBMap<_>> =
+            MapToCurveBasedHasher::new(b"sign").unwrap();
+        let fm: G2Affine = hasher.hash(message.as_ref()).unwrap();
+
+        todo!()
     }
 }
